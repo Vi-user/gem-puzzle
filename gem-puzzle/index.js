d8547dcb706b48432da56da03ace523d187b49c6
Vi-user <vi.volkova.2712@gmail.com>
2022-10-19 13:39:02 +0300

    feat: draw puzzle

--- gem-puzzle/index.js
+++ gem-puzzle/index.js
@@ -1,12 +1,13 @@
+console.log('index.js');
+/*=================GAME LOGIC===================*/
 const MATRIX_SIZE = 4;
-const EMPTY_TILE = 0;
+const EMPTY_TILE = '';
+const RIGHT_ORDER = makeMatrix(MATRIX_SIZE*MATRIX_SIZE );
 
-const exMatrix = [[1, 2, 3],[ 4, 0, 5],[ 6, 7, 8]];
-
-const makeMatrix = (n) => {
+function makeMatrix(n) {
     const array = [];
     for (let i = 1; i < n; i++) {
-        array.push(i);
+        array.push(i.toString());
     }
     array.push(EMPTY_TILE);
 
@@ -14,7 +15,7 @@ const makeMatrix = (n) => {
     const matrixSize = Math.sqrt(n);
 
     for (let i = 0; i < matrixSize; i++) {
-        matrix.push(array.slice(i*matrixSize, i*matrixSize+matrixSize))
+        matrix.push(array.slice(i * matrixSize, i * matrixSize + matrixSize))
     }
     return matrix;
 }
@@ -39,43 +40,98 @@ const swapElements = (matrix, a, b) => {
     [matrix[x1][y1], matrix[x2][y2]] = [matrix[x2][y2], matrix[x1][y1]]
 }
 
+const compareCoordinates = (a, b) => {
+    const
+        x1 = a[0],
+        y1 = a[1],
+        x2 = b[0],
+        y2 = b[1];
+    if ((x1 === x2 && (Math.abs(y1 - y2) === 1)) || (y1 === y2 && (Math.abs(x1 - x2) === 1))) {
+        return true
+    }
+    console.log('forbidden, curElCoordinates:', b)
+    return false
+}
+
 const tryToMove = (matrix, curEl) => {
     const emptyElCoordinates = findCoordinates(matrix, EMPTY_TILE)
     const curElCoordinates = findCoordinates(matrix, curEl);
-    //function to sravnit' coordinati
-    if (emptyElCoordinates[0] === curElCoordinates[0] && (Math.abs(emptyElCoordinates[1] - curElCoordinates[1])=== 1)) {
+    if (compareCoordinates(emptyElCoordinates, curElCoordinates)) {
         swapElements(matrix, curElCoordinates, emptyElCoordinates);
+        checkIfGameOver(matrix)
         console.log('can move, curElCoordinates:', curElCoordinates)
-        //
-        // return true;
+        return true;
     }
-    if (emptyElCoordinates[1] === curElCoordinates[1] && (Math.abs(emptyElCoordinates[0] - curElCoordinates[0])=== 1)) {
-        swapElements(matrix, curElCoordinates, emptyElCoordinates);
-        console.log('can move, curElCoordinates:', curElCoordinates)
-        // return true;
+    return false
+}
+
+const checkIfGameOver = (matrix) => {
+    if (JSON.stringify(matrix) === JSON.stringify(RIGHT_ORDER)) {
+        console.log("Hooray! You solved the puzzle in ##:## and N moves!")
     }
-    else {
-        console.log('forbidden, curElCoordinates:', curElCoordinates)
-        return false
-    }
+    // console.log(JSON.stringify(matrix))
+    // console.log(JSON.stringify(RIGHT_ORDER))
+}
+
+/*=================DRAW PUZZLE===================*/
+
+const createElem = (tag, ...classes) => {
+    const node = document.createElement(tag);
+    node.classList.add(...classes);
+    return node;
+}
+
+const highlightWrongElement = (curEl) => {
+    const items = document.querySelectorAll('.item');
+    const curItem = Array.from(items).find(el => el.textContent === curEl);
+    curItem.classList.add('warning')
+    setTimeout(function() {curItem.classList.remove('warning')} ,100)
+}
+
+const drawMatrix = (matrix) => {
+    console.log('hi');
+    const body = document.querySelector('body')
+    const wrapper = createElem('div', 'wrapper');
+    const squaresContainer = createElem('div', 'squares-container');
+    wrapper.append(squaresContainer)
+
+    matrix.forEach(matrixRow => {
+        const row = createElem('div', 'row');
+        squaresContainer.append(row);
+        matrixRow.forEach(el => {
+            // console.log('el', el);
+            const tile = createElem('div', 'item');
+            if (el === EMPTY_TILE) {
+
+                tile.classList.add('empty-tile');
+            }
+            tile.textContent = el;
+            row.append(tile)
+        })
+    })
+    body.append(wrapper)
 
 }
 
-// checkIfGameOver
 
-
+/*=================CONTROLLER===================*/
+const addTileClickHandler = (matrix) => {
+    document.querySelector('.squares-container').addEventListener('click', (e) => {
+        const value = e.target.innerText;
+        if (tryToMove(matrix, value)) {
+            document.querySelector('.wrapper').remove();
+            drawMatrix(matrix);
+            addTileClickHandler(matrix);
+        } else {
+            highlightWrongElement(value);
+        }
+    })
+}
 
 const startGame = () => {
     const matrix = makeMatrix(MATRIX_SIZE*MATRIX_SIZE);
+    drawMatrix(matrix);
+    addTileClickHandler(matrix);
 }
 
-
-console.log('canMove(12)', tryToMove(12));
-console.log('matrix AFTER', matrix)
-console.log('canMove(11)', tryToMove(11));
-console.log('matrix AFTER', matrix)
-console.log('canMove(2)', tryToMove(2));
-console.log('matrix AFTER', matrix)
-
-
-/*====================================*/
+startGame()
