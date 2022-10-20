75d0ef5c4b4fc87997b9cef6b7d68fc33816cfa6
Vi-user <vi.volkova.2712@gmail.com>
2022-10-20 12:55:13 +0300

    feat: buttons container, restart func, moves functional

--- gem-puzzle/index.js
+++ gem-puzzle/index.js
@@ -3,6 +3,7 @@ console.log('index.js');
 const MATRIX_SIZE = 4;
 const EMPTY_TILE = '';
 const RIGHT_ORDER = makeMatrix(MATRIX_SIZE*MATRIX_SIZE );
+// let steps = 0;
 
 const getRandomArbitrary = (min, max) => {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -26,7 +27,7 @@ function makeMatrix(n) {
 }
 
 const findCoordinates = (matrix, value) => {
-    console.log('findCoordinates', matrix, value)
+    // console.log('findCoordinates', matrix, value)
     for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[i].length; j++) {
             if (matrix[i][j] === value) {
@@ -38,7 +39,7 @@ const findCoordinates = (matrix, value) => {
 }
 
 const swapElements = (matrix, a, b) => {
-    console.log('swapElements', matrix, a, b)
+    // console.log('swapElements', matrix, a, b)
     const
         x1 = a[0],
         y1 = a[1],
@@ -86,6 +87,7 @@ const tryToMove = (matrix, curEl) => {
     const emptyElCoordinates = findCoordinates(matrix, EMPTY_TILE)
     const curElCoordinates = findCoordinates(matrix, curEl);
     if (compareCoordinates(emptyElCoordinates, curElCoordinates)) {
+        document.querySelector('.score-steps').textContent++;
         swapElements(matrix, curElCoordinates, emptyElCoordinates);
         checkIfGameOver(matrix)
         console.log('can move, curElCoordinates:', curElCoordinates)
@@ -102,7 +104,13 @@ const checkIfGameOver = (matrix) => {
 
 /*=================DRAW PUZZLE===================*/
 
-const createElem = (tag, ...classes) => {
+const drawWrapper = () => {
+    const body = document.querySelector('body')
+    const wrapper = createElement('div', 'wrapper');
+    body.append(wrapper)
+}
+
+const createElement = (tag, ...classes) => {
     const node = document.createElement(tag);
     node.classList.add(...classes);
     return node;
@@ -117,17 +125,19 @@ const highlightWrongElement = (curEl) => {
 
 const drawMatrix = (matrix) => {
     console.log('hi');
-    const body = document.querySelector('body')
-    const wrapper = createElem('div', 'wrapper');
-    const squaresContainer = createElem('div', 'squares-container');
+    // const body = document.querySelector('body')
+    // const wrapper = createElem('div', 'wrapper');
+    const wrapper = document.querySelector('.wrapper')
+
+    const squaresContainer = createElement('div', 'squares-container');
     wrapper.append(squaresContainer)
 
     matrix.forEach(matrixRow => {
-        const row = createElem('div', 'row');
+        const row = createElement('div', 'row');
         squaresContainer.append(row);
         matrixRow.forEach(el => {
             // console.log('el', el);
-            const tile = createElem('div', 'item');
+            const tile = createElement('div', 'item');
             if (el === EMPTY_TILE) {
 
                 tile.classList.add('empty-tile');
@@ -136,8 +146,47 @@ const drawMatrix = (matrix) => {
             row.append(tile)
         })
     })
-    body.append(wrapper)
+    // body.append(wrapper)
 
+}
+
+/*===================BUTTONS=====================*/
+
+const addButtons = () => {
+    const buttons = ['shuffle', 'stop', 'save', 'result']
+    const wrapper = document.querySelector('.wrapper');
+    const buttonsRow = createElement('div', 'buttons-row');
+
+    buttons.forEach(el => {
+        const button = createElement('button', 'button', `button-${el}`);
+
+        button.innerText = (el === 'shuffle') ? `${el} & start` : el;
+        // button.addEventListener('click', makeShuffle)
+        // button.addEventListener('click', `make${el}`)
+        buttonsRow.append(button);
+    })
+    wrapper.append(buttonsRow);
+}
+
+
+/*===================Time & Score=====================*/
+
+function addTimeScore() {
+    const wrapper = document.querySelector('.wrapper');
+    const timeScoreRow = createElement('div', 'time-score-row');
+//SCORE
+    const scoreContainer = createElement('div', 'score-container');
+    const scoreTitle = createElement('span', 'score-title');
+    const scoreSteps = createElement('span', 'score-steps');
+    scoreTitle.textContent = `Moves: `;
+    scoreSteps.textContent = 0;
+    scoreContainer.append(scoreTitle, scoreSteps);
+    wrapper.append(scoreContainer);
+//TIMER
+
+
+
+    wrapper.append(timeScoreRow);
 }
 
 
@@ -146,7 +195,7 @@ const addTileClickHandler = (matrix) => {
     document.querySelector('.squares-container').addEventListener('click', (e) => {
         const value = e.target.innerText;
         if (tryToMove(matrix, value)) {
-            document.querySelector('.wrapper').remove();
+            document.querySelector('.squares-container').remove();
             drawMatrix(matrix);
             addTileClickHandler(matrix);
         } else {
@@ -156,8 +205,12 @@ const addTileClickHandler = (matrix) => {
 }
 
 const startGame = () => {
+
     const matrix = makeMatrix(MATRIX_SIZE*MATRIX_SIZE);
     shuffleMatrix(matrix);
+    drawWrapper();
+    addButtons();
+    addTimeScore();
     drawMatrix(matrix);
     addTileClickHandler(matrix);
 }
@@ -178,3 +231,19 @@ startGame()
 // console.log('matrix AFTER', matrix)
 // console.log('canMove(2)', tryToMove(2));
 // console.log('matrix AFTER', matrix)
+
+const restartGame = () => {
+    console.log('restartGame')
+    document.querySelector('.squares-container').remove();
+    const matrix = makeMatrix(MATRIX_SIZE*MATRIX_SIZE);
+    shuffleMatrix(matrix);
+    drawMatrix(matrix);
+    addTileClickHandler(matrix);
+    document.querySelector('.score-steps').textContent = 0;
+    // timer = 0
+}
+
+window.onload = function () {
+    const shuffleBtn = document.querySelector('.button-shuffle');
+    shuffleBtn.addEventListener('click', restartGame)
+}
